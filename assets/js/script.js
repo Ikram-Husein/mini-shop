c65b7c6c103e02538374c5f58ad728936726857c
Ikram Husein <ikramhusain.dev@gmail.com>
2022-12-31 13:04:38 +0600

    2nd commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const mobile_filter_btn = document.querySelector(".mobile_filter_btn"),
 	filter_btn = document.querySelector(".filter_btn"),
 	mobile_siderbar = document.querySelector("#mobile_siderbar"),
 	focus_filter = document.querySelector(".focus_filter"),
+	overlay = document.querySelector("#overlay"),
 	body = document.querySelector("body");
 
 filter_btn.addEventListener("click", () => {
@@ -9,6 +10,7 @@ filter_btn.addEventListener("click", () => {
   mobile_siderbar.style.transition = "0.2s";
 	mobile_siderbar.style.right = "0";
 	focus_filter.style.borderColor = "#f43f5e";
+	overlay.style.visibility = "visible";
 });
 
 mobile_filter_btn.addEventListener("click", () => {
@@ -16,4 +18,12 @@ mobile_filter_btn.addEventListener("click", () => {
 	mobile_siderbar.style.transition = "0.2s";
 	mobile_siderbar.style.right = "-100%";
 	focus_filter.style.borderColor = "#d1d5db";
+	overlay.style.visibility = "hidden";
 });
+
+overlay.addEventListener('click', () => {
+	mobile_siderbar.style.visibility = "hidden";
+	mobile_siderbar.style.transition = "0.2s";
+	mobile_siderbar.style.right = "-100%";
+	overlay.style.visibility = "hidden";
+})
